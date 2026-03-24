3be3f8eb22f9c3ce33678cce38022aeb2eba684c
Pritesh Shetty <priteshshetty89@gmail.com>
2026-03-24 19:38:40 +0000

    overlays: Fix focus behaviour for drafts overlay.
    Previously, focus could escape the drafts overlay when moving
    backward through focusable elements. This commit adds keydown
    handler to cycle focus within the drafts overlay when navigating.
    Wrap focus between first and last elements and redirect focus back
    into the overlay if it moves outside the focusable elements.
    
    Fixes part of #26941.
    
    Co-Authored-By: Pratik Chanda <pratikchanda2000@gmail.com>

--- web/src/overlay_util.ts
+++ web/src/overlay_util.ts
@@ -15,14 +15,16 @@ export function enable_scrolling(): void {
 }
 
 export const OVERLAY_FOCUSABLE_SELECTOR =
-    "input, button, select, .input, .sidebar-item, .ind-tab.first, a[href], a[tabindex='0']";
+    "input, button, select, .input, .sidebar-item, .ind-tab.first, a[href], a[tabindex='0'], .overlay-message-info-box";
 
 export function get_visible_focusable_elements_in_overlay_container(
     $container: JQuery,
 ): HTMLElement[] {
     const visible_focusable_elements = [...$(OVERLAY_FOCUSABLE_SELECTOR, $container)].filter(
         (element) =>
-            element.getClientRects().length > 0 && $(element).css("visibility") !== "hidden",
+            element.getClientRects().length > 0 &&
+            $(element).css("visibility") !== "hidden" &&
+            !$(element).is(":disabled"),
     );
     return visible_focusable_elements;
 }
